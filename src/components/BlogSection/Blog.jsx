36827d624ef3d7607de2f87f77309be838074453
Nikhil Dhariwal <nicksd111@gmail.com>
2024-08-09 19:43:49 +0530

    Update Blog.jsx

--- src/components/BlogSection/Blog.jsx
+++ src/components/BlogSection/Blog.jsx
@@ -131,7 +131,7 @@ const Blog = () => {
       repo="404Nikhil/Anubhav-frontend"
       repoId="R_kgDOMJ3j1g"
       category="Announcements"
-      categoryId="DIC_kwDOF1L2fM4B-hVS"
+      categoryId="DIC_kwDOMJ3j1s4CgbtK"
       mapping="pathname"
       term="Welcome to @giscus/react component!"
       reactionsEnabled="1"
